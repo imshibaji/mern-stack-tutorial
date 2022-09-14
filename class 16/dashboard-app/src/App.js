5d04b670cf6f352727a2dd34d902853faf8e2b43
Shibaji Debnath <imshibaji@gmail.com>
2022-09-14 21:29:57 +0530

    ReactJS Training initaiate

---
+++ class 16/dashboard-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './common/navbar';
+
+function App() {
+  return(
+    <div>
+      <Navbar />
+      <div className="container">
+        <h1>Welcome to React Js</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
